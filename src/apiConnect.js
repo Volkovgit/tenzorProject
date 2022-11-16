5c5e1b2828417cb43cab19d72d3012293ebaa590
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-16 23:45:22 +0300

    Добавил класс http запросов

---
+++ src/apiConnect.js
@@ -0,0 +1,45 @@
+function LastFmRequests() {
+  const lasFm = {
+    url: "http://ws.audioscrobbler.com/2.0",
+    apiKey: "2cf860f449f80740452bbcee1d7a742e",
+    apiSecret: "4e692081acc722a1e1fedd990e251fe6",
+  };
+  const createUrl = (params) => {
+    const url = new URL(lasFm.url);
+    for (key in params) {
+      url.searchParams.set(key, params[key]);
+    }
+    return url;
+  };
+
+  const createRequest = (url) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    return xhr;
+  };
+
+  function request(params) {
+    return new Promise((resolve, reject) => {
+      const url = createUrl(params);
+      const xhr = createRequest(url);
+      xhr.send();
+      xhr.onload = () => {
+        resolve(JSON.parse(xhr.response));
+      };
+    });
+  }
+  return {
+    request,
+  };
+}
+
+// const testObj = {
+//   method: "chart.getTopArtists",
+//   format: "json",
+//   api_key: lasFm.apiKey,
+// };
+
+// LastFmRequests()
+//   .request(testObj)
+//   .then((data) => console.log(data));
