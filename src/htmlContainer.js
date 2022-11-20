a367afd33455d6533aac6ac02de9f13f36df4922
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-20 13:27:14 +0300

    Добавил доку для функций

--- src/htmlContainer.js
+++ src/htmlContainer.js
@@ -1,6 +1,14 @@
 import { lasFmInterface } from "./lasFmInterface.js";
 
+/**
+ * Изоляция внутренних функций и преобразования страницы
+ */
 export function htmlContainer() {
+  /**
+   * Генерируем массив готовых карточек всех исполнителей
+   * @param {array} artists - список акртистов
+   * @returns {array}
+   */
   function createArtistCardsList(artists) {
     artists = artists.map((artist) => {
       const div = document.createElement("div");
@@ -15,11 +23,16 @@ export function htmlContainer() {
     return artists;
   }
 
-  async function setArtistTagsInArtistCard(artists,artistCount,tagCount) {
-    artists = artists.artists.artist.slice(0, artistCount);
+  /**
+   * Каждому артисту запрашиваем его топ тегов
+   * @param {object} artists - список акртистов в JSON формате
+   * @param {number} tagCount - кол-во тегов которые нужно вывести
+   * @returns {array}
+   */
+  async function setArtistTagsInArtistCard(artists, tagCount) {
     const artistWithTags = await Promise.all(
       artists.map(async (artist) => {
-        await getArtistTags(artist.name).then((tags) => {
+        await getTagsByArtistName(artist.name).then((tags) => {
           artist.toptags = tags.toptags.tag
             .slice(0, tagCount)
             .map((tag) => tag.name);
@@ -30,17 +43,39 @@ export function htmlContainer() {
     return artistWithTags;
   }
 
-  async function getArtistTags(name) {
+  function sliceArtistArray(artists, artistCount) {
+    return artists.artists.artist.slice(0, artistCount);
+  }
+
+  function sliceTracksArray(tracks, tracksCount) {
+    return tracks.tracks.track.slice(0, tracksCount);
+  }
+
+  /**
+   * Запрашиваем топ популярных тегов артиста
+   * @param {string} name - Имя артиста
+   * @returns {array}
+   */
+  async function getTagsByArtistName(name) {
     return await lasFmInterface().getTopAuthorsTags(name);
   }
 
+  /**
+   * Вставляем на страницу готовый массив элементов
+   * @param {string} tagName - контейнер куда необходимо вставить готовые элементы
+   * @param {array} elements - массив эелементво для вставки на страницу
+   */
   function insertElementsIntoHtmlContainer(tagName, elements) {
     const container = document.getElementsByClassName(`${tagName}`)[0];
     elements.forEach((element) => {
       container.appendChild(element);
     });
   }
-
+  /**
+   * Генерация массива карточек треков
+   * @param {array} tracks - массив треков
+   * @returns {array}
+   */
   function createTrackCardList(tracks) {
     tracks = tracks.map((track) => {
       const div = document.createElement("div");
@@ -58,12 +93,16 @@ export function htmlContainer() {
     return tracks;
   }
 
-  async function setArtistTagInTrackCard(tracks) {
-    const tagCount = 1;
-    tracks = tracks.tracks.track.slice(0, 24);
+  /**
+   * Каждому треку запрашиваем его топ тегов
+   * @param {array} tracks - список треков
+   * @param {number} tagCount - кол-во тегов которые нужно вывести
+   * @returns {array}
+   */
+  async function setArtistTagInTrackCard(tracks, tagCount) {
     const tracksWithArtistTag = await Promise.all(
       tracks.map(async (track) => {
-        await getArtistTags(track.artist.name).then((tags) => {
+        await getTagsByArtistName(track.artist.name).then((tags) => {
           track.toptags = tags.toptags.tag
             .slice(0, tagCount)
             .map((tag) => tag.name);
@@ -73,21 +112,27 @@ export function htmlContainer() {
     );
     return tracksWithArtistTag;
   }
-
+  /**
+   * Агрегация вышестоящих функций. Подготовка и вставка карточек артистов
+   */
   function insertArtistIntoHTML() {
     lasFmInterface()
       .getTopArtist()
-      .then((artists) => setArtistTagsInArtistCard(artists,10,2))
+      .then((artists) => sliceArtistArray(artists, 10))
+      .then((artists) => setArtistTagsInArtistCard(artists, 2))
       .then((artists) => createArtistCardsList(artists))
       .then((artists) =>
         insertElementsIntoHtmlContainer("artist_card_list_container", artists)
       );
   }
-
+  /**
+   * Агрегация вышестоящих функций. Подготовка и вставка карточек треков
+   */
   function insertTracksIntoHtml() {
     lasFmInterface()
       .getTopTracks()
-      .then((tracks) => setArtistTagInTrackCard(tracks))
+      .then((tracks) => sliceTracksArray(tracks, 24))
+      .then((tracks) => setArtistTagInTrackCard(tracks, 10))
       .then((tracks) => createTrackCardList(tracks))
       .then((tracks) =>
         insertElementsIntoHtmlContainer("card_container", tracks)
